d96023e16467ebaf0c7c9425e2a11ce831690c50
daniil <tarasenkodaniilromanovich@gmail.com>
2023-06-19 19:54:43 +0300

    Тест 2

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,64 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import Users from './Elements/UsersList.jsx'
+import Albums from './Elements/Albums.jsx'
+import Photos from './Elements/Photos.jsx'
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom'
 
-const UserList = () => {
-  const [users, setUsers] = useState([]);
-  const [albums, setAlbums] = useState([]);
-  const [photos, setPhotos] = useState([]);
-
+function App() {
+  const [users, setUsers] = useState([])
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetchUsers()
+  }, [])
+  const fetchUsers = async () => {
+    await fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((data) => setUsers(data))
-      .catch((error) => console.log(error));
-  }, []);
+      .catch((error) => {
+        console.log('ERROR:' + error)
+      })
+  }
 
-  const fetchAlbums = (userId) => {
-    fetch(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`)
+  const [albums, setAlbums] = useState([])
+  useEffect(() => {
+    fetchAlbums()
+  }, [])
+  const fetchAlbums = async () => {
+    await fetch('https://jsonplaceholder.typicode.com/albums')
       .then((response) => response.json())
       .then((data) => setAlbums(data))
-      .catch((error) => console.log(error));
-  };
+      .catch((error) => {
+        console.log('ERROR:' + error)
+      })
+  }
 
-  const fetchPhotos = (albumId) => {
-    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+  const [photos, setPhotos] = useState([])
+  useEffect(() => {
+    fetchPhotos()
+  }, [])
+  const fetchPhotos = async () => {
+    await fetch('https://jsonplaceholder.typicode.com/photos')
       .then((response) => response.json())
       .then((data) => setPhotos(data))
-      .catch((error) => console.log(error));
-  };
+      .catch((error) => {
+        console.log('ERROR:' + error)
+      })
+  }
 
   return (
-    <div className='main'>
-      <h1>User List</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name}
-            <button className='btn' onClick={() => fetchAlbums(user.id)}>Albums</button>
-            {albums.length > 0 && (
-              <ul className='ul-albums'>
-                {albums.map((album) => (
-                  <li key={album.id}>
-                    {album.title}
-                    <button onClick={() => fetchPhotos(album.id)}>Photos</button>
-                    {photos.length > 0 && (
-                      <ul className='ul-photos'>
-                        {photos.map((photo) => (
-                          <li className='li-photos' key={photo.id}>
-                            <img src={photo.thumbnailUrl} alt={photo.title} />
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>
+    <div className="App">
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Users users={users} />} />
+            <Route path="/:id" element={<Albums albums={albums} />} />
+            <Route path="/:id/:slug" element={<Photos photos={photos} />} />
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
     </div>
-  );
-};
+  )
+}
 
-export default UserList;
+export default App
---
+++ react-router/src/Elements/Photos.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Photos.css';
+import { useParams } from 'react-router-dom';
+
+function Photos({ photos }) {
+  let { slug } = useParams();
+  let albumPhotos = [];
+  photos.forEach(element => {
+    if (element.albumId == slug) {
+      albumPhotos.push(element);
+    }
+  });
+  return (
+    <div className='photos'>
+      {albumPhotos && albumPhotos.map((user, index) => {
+        return (
+          <div className="albumPhotos" key={index}>
+            <p>id: {user.id}</p>
+            <img src={user.url} />
+            <img src={user.thumbnailUrl} />
+            <p>title: {user.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  )
+}
+
+export default Photos;
